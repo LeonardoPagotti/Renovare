69070a4e38d4fac22aaf80428b106913bca5afbc
LeonardoPagotti <leonardodpagotti@hotmail.com>
2022-09-20 16:42:31 -0300

    sexto commit

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const filterBtn = document.querySelector("#filter-select");
 
 let oldInputValue;
 
-// Funções
+
 const saveTodo = (text, done = 0, save = 1) => {
   const todo = document.createElement("div");
   todo.classList.add("todo");
